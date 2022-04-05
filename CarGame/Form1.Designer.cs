d0a0383a257b304f7801d1f0974b1835352b2792
김이영 <kly1109@eghis.co.kr>
2022-04-05 17:49:11 +0900

    초기 커밋.

---
+++ CarGame/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace CarGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlRoad = new System.Windows.Forms.Panel();
+            this.picCar = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.tmrMove = new System.Windows.Forms.Timer(this.components);
+            this.playTimer = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.pnlRoad.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCar)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.60516F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.39483F));
+            this.tableLayoutPanel1.Controls.Add(this.pnlRoad, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(614, 543);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pnlRoad
+            // 
+            this.pnlRoad.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pnlRoad.Controls.Add(this.picCar);
+            this.pnlRoad.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlRoad.Location = new System.Drawing.Point(3, 3);
+            this.pnlRoad.Name = "pnlRoad";
+            this.pnlRoad.Size = new System.Drawing.Size(402, 537);
+            this.pnlRoad.TabIndex = 0;
+            // 
+            // picCar
+            // 
+            this.picCar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.picCar.BackColor = System.Drawing.Color.DimGray;
+            this.picCar.Image = global::CarGame.Properties.Resources.carImage;
+            this.picCar.Location = new System.Drawing.Point(135, 401);
+            this.picCar.Name = "picCar";
+            this.picCar.Size = new System.Drawing.Size(114, 113);
+            this.picCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCar.TabIndex = 0;
+            this.picCar.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(411, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 31.0987F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 68.90131F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(200, 537);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(1, 167);
+            this.panel2.Margin = new System.Windows.Forms.Padding(1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(198, 369);
+            this.panel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 44.94949F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55.05051F));
+            this.tableLayoutPanel3.Controls.Add(this.btnStart, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(this.lblTime, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.lblScore, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.button2, 1, 3);
+            this.tableLayoutPanel3.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(1, 1);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 4;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(198, 164);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.LightGray;
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(1, 85);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(1);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(86, 78);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "게임시작!";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTime.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTime.Location = new System.Drawing.Point(91, 59);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(3);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(104, 22);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(1, 1);
+            this.label1.Margin = new System.Windows.Forms.Padding(1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "유저명";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblScore.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblScore.Location = new System.Drawing.Point(91, 31);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(3);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(104, 22);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(1, 57);
+            this.label3.Margin = new System.Windows.Forms.Padding(1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "플레이시간";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(1, 29);
+            this.label2.Margin = new System.Windows.Forms.Padding(1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "점수";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(89, 85);
+            this.button2.Margin = new System.Windows.Forms.Padding(1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 78);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "저장";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(91, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Interval = 3000;
+            // 
+            // tmrMove
+            // 
+            this.tmrMove.Interval = 300;
+            // 
+            // playTimer
+            // 
+            this.playTimer.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 543);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.pnlRoad.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picCar)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel pnlRoad;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox picCar;
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.Timer tmrMove;
+        private System.Windows.Forms.Timer playTimer;
+    }
+}
+
---
+++ CarGame/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarGame
+{
+    public partial class Form1 : Form
+    {
+        int _score = 0;
+        bool reButton = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += Form1_KeyDown;
+            this.KeyPreview = true;
+            tmrMain.Tick += TmrMain_Tick;
+            btnStart.Click += BtnStart_Click;
+            tmrMove.Tick += TmrMove_Tick;
+            playTimer.Tick += PlayTimer_Tick;
+        }
+
+        private void PlayTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TmrMove_Tick(object sender, EventArgs e)
+        {
+            MoveObstacle();
+        }
+
+        private void TmrMain_Tick(object sender, EventArgs e)
+        {
+            CreateObstacle();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            lblTime.Text = "00:00:00";
+            timer.Tick += new EventHandler(PlayTimer_Tick);
+            timer.Start();
+
+
+            if (reButton)
+            {
+                Button clickedButton = (Button)sender;
+                clickedButton.Text = "게임중지";
+
+                tmrMain.Start();
+                tmrMove.Start();
+
+                reButton = false;
+            }
+            else
+            {
+                Button clickedButton = (Button)sender;
+                clickedButton.Text = "게임시작";
+
+                tmrMain.Stop();
+                tmrMove.Stop();
+                
+                reButton = true;
+            }
+            
+        }
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                picCar.Left -= 4;
+            }
+            else if(e.KeyCode == Keys.Right)
+            {
+                picCar.Left += 4;
+            }
+        }
+
+        private void CreateObstacle()
+        {
+            PictureBox box = new PictureBox();
+            box.Size = new Size(60, 60);
+            box.Tag = 0;
+
+            int max = pnlRoad.Width - box.Height; 
+            Random random = new Random();
+            box.Left = random.Next(1, max);
+            box.BackColor = Color.Red;
+            pnlRoad.Controls.Add(box);
+
+        }
+        private void MoveObstacle()
+        {
+            foreach (Control item in pnlRoad.Controls)
+            {
+                bool isGetScore = item.Top >= (picCar.Top + picCar.Height);
+                bool isNowScore = Convert.ToInt32(item.Tag) == 0;
+
+                if (item.Top > pnlRoad.Height) item.Dispose();
+
+                if (isGetScore && isNowScore)
+                {
+                    item.Tag = 1;
+                    _score++;
+                    lblScore.Text = _score.ToString();
+                }
+
+                if (item.Name == picCar.Name) continue;
+                item.Top += 10;
+            }
+        }
+
+        private void CheckCrush()
+        {
+            int carL = picCar.Left;
+            int carR = carL + picCar.Width;
+
+            int carT = picCar.Top;
+            int carB = carT + picCar.Height;
+
+            foreach(Control item in pnlRoad.Controls)
+            {
+                if (Convert.ToInt32(item.Tag) != 0) continue;
+
+                int obstacleL = item.Left;
+                int obstacleR = obstacleL + item.Width;
+
+                int obstacleT = item.Top;
+                int obstacleB = obstacleT + item.Height;
+
+                if(obstacleL >= carL && obstacleL <= carR)
+                {
+                    tmrMain.Stop();
+                    tmrMove.Stop();
+                }
+                if(obstacleR >= carL && obstacleR <= carR)
+                {
+                    tmrMain.Stop();
+                    tmrMove.Stop();
+                }
+
+            }
+        }
+    }
+}
